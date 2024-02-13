211bfca1199586476921e64c27165a163c53e9f0
sachin <contact2sachin11@gmail.com>
2024-02-13 19:03:32 +0530

    added new projects

--- SparrowBank6/src/main/java/com/sparrowbank/config/ProjectSecurityConfig.java
+++ SparrowBank6/src/main/java/com/sparrowbank/config/ProjectSecurityConfig.java
@@ -25,8 +25,10 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount", "/myCard", "/myBalance","/myLoan").authenticated());
         http.authorizeHttpRequests((requests) -> requests.requestMatchers("/notices", "/contact").permitAll());
+
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+
         return http.build();
     }
     /*@Bean
