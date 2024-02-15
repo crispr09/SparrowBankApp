c2568197f70970de84855837f13ed2446ca5887b
sachin <contact2sachin11@gmail.com>
2024-02-15 14:18:13 +0530

    added project for BCryptPasswordEncoder

---
+++ SparrowBank7/src/main/java/com/sparrowbank/entity/Customer.java
@@ -0,0 +1,28 @@
+package com.sparrowbank.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String email;
+    private String password;
+    private String role;
+
+
+}
